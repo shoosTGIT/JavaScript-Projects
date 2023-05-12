b35c338c176107add9717ea61ed66ce76d624593
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2023-05-12 16:09:39 -0400

    Fixes and new files committed

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,15 @@
 function My_First_Function() {                  //defining my function
     var str = "This is the button text!";       //setting the equation up
-    document.getElementByID("Button_Text").innerHTML = str;     //allowing the string to react to the button press
+    document.getElementById("Button_Text").innerHTML = str;     //allowing the string to react to the button press
 }
 
 function myFunction() {
     var sentence = "I am learning";
     sentence += " a lot from this book!";
-    document.getElementByID("Concatenate").innerHTML = sentence;
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
+
+function myFunction1(p1, p2) {
+    return p1 * p2;
+}
+  
---
+++ Basic JavaScript Projects/Project3_math_operations/JS/main.js
@@ -0,0 +1,43 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 =" + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication() {
+    var simple_math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 * 8 =" + simple_math;
+}
+
+function division() {
+    var simple_math = 48 / 6;
+    document.getElementById("Math").innerHTML = " 48 / 6 =" + simple_math;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math; 
+}
+
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math").innerHTML = -x;
+}
+
+var X = 5;
+X++;
+document.write(X);
+
+var Y = 5.25;
+Y--;
+document.write(Y);
