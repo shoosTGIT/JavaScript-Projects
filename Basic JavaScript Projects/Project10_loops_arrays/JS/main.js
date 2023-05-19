9c87b784aefeb543a318038fe0973df983206930
Mitchell Youssef <129881959+shoosTGIT@users.noreply.github.com>
2023-05-19 14:09:22 -0400

    Project10

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -47,3 +47,43 @@ function constant_function() {
     Musical_Instrument.price = "$900";
     document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;
 }
+
+var L = 82;
+document.write("<br>" + L);
+{
+    let L = 33;
+    document.write("<br>" + L);
+}
+document.write("<br>" + L);
+
+document.getElementById("demo").innerHTML = myFunction("John");
+
+function myFunction(name) {
+  return "Hello " + name;
+}
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description: function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+let next = "";
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { break; }
+    next += "The number is " + i + "<br>";
+}
+
+document.getElementById("break").innerHTML = next;
+
+let lext = "";
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { continue; }
+    lext += "The number is " + i + "<br>";
+  }
+  document.getElementById("lext").innerHTML = text;
